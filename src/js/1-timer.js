f06b557d00efa5e06514ab7f290fad4eeaf16100
Volodimir <sanin.vova200@gmail.com>
2025-07-14 00:57:33 +0300

    make part of first task

---
+++ src/js/1-timer.js
@@ -0,0 +1,37 @@
+`use strict`;
+
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css"
+
+
+const input = document.querySelector("#datetime-picker");
+const startBtn = document.querySelector("[data-start]");
+
+startBtn.disabled = true;
+
+let userSelectedDate = null;
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        console.log(selectedDates[0]);
+        const selectedDate = selectedDates[0];
+        userSelectedDate = selectedDate;
+        const currentDate = new Date();
+        console.log("Обрана дата:", userSelectedDate);
+
+
+        if (selectedDate <= currentDate) {
+            window.alert("Please choose a date in the future");
+            startBtn.disabled = true;
+        } else {
+            startBtn.disabled = false;
+        }
+    },
+};
+  
+flatpickr(input, options);
+
+
